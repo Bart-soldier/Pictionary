db67d3b77caf15992073debf87b96ce3fbd35ce4
root <root@Billy.localdomain>
2020-04-04 18:03:29 +0200

    Reload page when changing route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Home from './components/views/Home';
-import Play from './components/views/Play';
-import Error from './components/views/Error';
-import Navigation from './components/views/Navigation';
+import Home from './views/Home';
+import Play from './views/Play';
+import Error from './views/Error';
+import Navigation from './views/Navigation';
 
 class App extends Component {
   render() {
     return (
-       <BrowserRouter>
+       <BrowserRouter forceRefresh={true}>
         <div>
           <Navigation />
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/play" component={Play}/>
              <Route path='*' component={Error} />
-            <Route component={Error}/>
            </Switch>
         </div>
       </BrowserRouter>
